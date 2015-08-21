05e51d44afe622720e5ed7c4a2c1435987f9da01
Stan <kulyavtsev@yahoo.com>
2015-08-21 11:34:28 -0700

    Add video_activity to video

--- app/models/video.rb
+++ app/models/video.rb
@@ -1,5 +1,6 @@
 class Video < ActiveRecord::Base
   belongs_to :customer
+  has_many :video_activities
 
   validates :subject, presence: true
   validates :url, presence: true
--- spec/models/video_spec.rb
+++ spec/models/video_spec.rb
@@ -4,4 +4,5 @@ RSpec.describe Video, type: :model do
   it { is_expected.to validate_presence_of :subject }
   it { is_expected.to validate_presence_of :url }
   it { is_expected.to belong_to :customer }
+  it { is_expected.to have_many :video_activities }
 end
